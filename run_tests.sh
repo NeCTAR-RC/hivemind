1ccb33bef03be5deb292ee9974b04acda89a8fb8
Russell Sim <russell.sim@gmail.com>
2013-10-02 15:19:06 +1000

    Added some initial test cases for disabling nova
    Change-Id: Ic1c1d70f6efae3e968fec6401897ac4d9f6457a5

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -x
+
+virtualenv .
+
+source bin/activate
+
+pip install -r requirements.txt
+pip install -e .
+
 RETURN=0
 
 for file in $(find . -name \*.py)
@@ -10,5 +19,11 @@ do
         RETURN=1
     fi
 done
+
+./bin/nosetests -v
+if [ ! $? -eq 0 ]
+then
+    RETURN=1
+fi
 
 exit $RETURN
