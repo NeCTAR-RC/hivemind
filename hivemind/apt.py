ed998c9bc3eddb5f398e4536541e8d1958d377e3
Devendran Jagadisan <devendran.jagadisan@gmail.com>
2013-10-18 12:00:06 +1100

    functions for swift upgrades
    Change-Id: I0508bfcd11cdfa2b78c786241c5df4cb4915367f

--- hivemind/apt.py
+++ hivemind/apt.py
@@ -12,7 +12,9 @@ package_version = re.compile(r'   ([^\s]+) \(([^\s]+) => ([^\s]+)\)')
 
 @parallel(pool_size=20)
 def update():
-    run("apt-get update")
+    #update to get latest list
+    results = run("apt-get update")
+    return results
 
 
 @parallel(pool_size=10)
@@ -39,6 +41,24 @@ def upgrade(packages=[]):
         puppet.enable_agent()
     nova.enable_host_services()
     nagios.cancel_host_maintence(outage)
+
+
+def get_upgrade_shell(packages, option=None):
+        with shell_env(DEBIAN_FRONTEND='non-interactive'):
+            if option is None:
+                run("apt-get install -o Dpkg::Options::='--force-confold' %s" %
+                    " ".join(packages)
+                    )
+            elif option is 'overide':
+                run("apt-get install -o Dpkg::Options::='--force-confdef' %s" %
+                    " ".join(packages)
+                    )
+
+
+@parallel(pool_size=20)
+def update_packages():
+    result = run("apt-get upgrade --assume-no -V", warn_only=True, quiet=True)
+    return result
 
 
 @parallel(pool_size=20)
@@ -69,7 +89,7 @@ def print_changes(host_packages):
     packages = list(set(chain(*[packages.keys()
                                 for packages in host_packages.values()])))
     packages.sort()
-    puts("\nThe following packages will be upgraded:")
+    puts("\nThe following packages will be upgraded")
     for package in packages:
         package_versions = [p.get(package) for p in host_packages.values()]
         package_versions = [pv for pv in package_versions if pv is not None]
