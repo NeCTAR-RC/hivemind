bb376376e9b5cc9dc65c04b37bb8d561a288c46a
Russell Sim <russell.sim@gmail.com>
2013-11-01 14:35:49 +1100

    Remove default file logging
    This should be specified in an rc file.
    
    Change-Id: I9fc79dbff9953e42a392dbfbdde05d432de01f1e

--- hivemind/log.py
+++ hivemind/log.py
@@ -1,5 +1,4 @@
 import re
-import os.path
 import logging
 import logging.handlers
 from collections import deque
@@ -11,19 +10,11 @@ console = logging.StreamHandler()
 console.setLevel(logging.ERROR)
 logging.root.addHandler(console)
 
-handler = logging.handlers.RotatingFileHandler(
-    os.path.expanduser('~/.sixpack.log'),
-    maxBytes=10 * 1024 * 1024,
-    backupCount=5)
-
 logger = logging.getLogger('paramiko.transport')
 logger.setLevel(logging.WARNING)
 logger = logging.getLogger('paramiko.hostkeys')
 logger.setLevel(logging.WARNING)
 
-formatter = logging.Formatter('%(asctime)s %(name)-35s: %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logging.root.addHandler(handler)
 logging.root.setLevel(logging.INFO)
 ansi_escape = re.compile(r'\x1b[^m]*m')
 
