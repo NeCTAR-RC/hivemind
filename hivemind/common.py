8740a70a0aab456712fa9511ebcb66dbde9d1e2e
Kieran Spear <kispear@gmail.com>
2013-11-10 18:05:17 +1100

    Use entry_points to collect fabric tasks
    Change-Id: I2d8ffcc2502dc85b6fa2e02dc7ffcc6205c2650e

--- hivemind/common.py
+++ hivemind/common.py
@@ -1,8 +1,24 @@
 import sys
+
 from fabric.api import env, puts
+from fabric import main as fabric_main
+import pkg_resources
 
 
-def main(filename):
+def _load_fabfile(path, importer=None):
+    all_tasks = {}
+    for entrypoint in pkg_resources.iter_entry_points(group='hivemind.tasks'):
+        plugin = entrypoint.load()
+        tasks = _load_tasks(plugin)
+        all_tasks[entrypoint.name] = tasks
+    return None, all_tasks, None
+
+
+def _load_tasks(imported):
+    return fabric_main.extract_tasks(vars(imported).items())[0]
+
+
+def main(filename=__file__):
     # work around for when python tries to use the byte compile version of the
     # fabfile.
     if filename.endswith('.pyc'):
@@ -16,5 +32,5 @@ def main(filename):
                 puts("    %-15s %s" % (role, ",".join(host_list)))
         sys.argv = sys.argv + ["-l"]
 
-    from fabric.main import main
-    main()
+    fabric_main.load_fabfile = _load_fabfile
+    fabric_main.main()
