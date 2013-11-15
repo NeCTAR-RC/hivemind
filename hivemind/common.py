48e50d9e59051ed04f2d38c344e866a181f6fc37
Kieran Spear <kispear@gmail.com>
2013-11-16 00:01:33 +1100

    Support config file format for hivemindrc
    Change-Id: Ia44fbf8ebb00894e1e96b90caf9d0dcdfe1484a0

--- hivemind/common.py
+++ hivemind/common.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import ConfigParser
 from os import path
 import argparse
 import inspect
@@ -12,6 +13,9 @@ import fabric.state
 import fabric.utils
 from fabric.api import env, puts, output, execute
 from fabric.api import task as fabric_task
+
+
+CONF = ConfigParser.ConfigParser()
 
 
 def _load_fabfile(path=None, importer=None):
@@ -218,7 +222,15 @@ def load_rc(program_name):
     progrc = "." + program_name + "rc"
     filename = path.expanduser(path.join("~", progrc))
     if path.exists(filename):
-        execfile(filename, globals())
+        try:
+            load_config(filename)
+        except:
+            execfile(filename, globals())
+
+
+def load_config(filename):
+    conf = CONF
+    conf.read(filename)
 
 
 def load_subcommands(mapping, parser, prefix=""):
@@ -271,11 +283,12 @@ def main_plus():
 
     # Setup environment
     set_defaults()
-    load_rc(parser.prog)
 
     # Load tasks
     docstring, callables, default = _load_fabfile()
     fabric.state.commands.update(callables)
+
+    load_rc(parser.prog)
 
     # Register subcommands
     load_subcommands(fabric.state.commands, subparsers)
