0bd140dd16e82f9c97915f4d2266a5f37458a11d
Russell Sim <russell.sim@gmail.com>
2014-05-16 09:16:15 +1000

    Added gerrit config branch tasks
    Change-Id: I73d609ae7fc59f4dfcec0cac4b4fd10085539c8c

--- hivemind/gerrit.py
+++ hivemind/gerrit.py
@@ -2,7 +2,7 @@ import os
 from fabric.api import (env, task, local)
 
 from hivemind.decorators import verbose
-from hivemind import util
+from hivemind import util, git
 
 
 def gitreview_username():
@@ -39,8 +39,47 @@ def create(name):
 
 @task
 @verbose
+def clone(project_name):
+    """Clone a repository from gerrit.
+
+       :param str project_name: The name of the repository you wish to
+         clone. (e.g NeCTAR-RC/puppet-openstack)
+
+    """
+    user = gitreview_username()
+    project = project_name.split('/')[-1]
+    local('git clone ssh://%s@review.rc.nectar.org.au:29418/%s %s' % (
+          user, project_name, project))
+
+
+@task
+@verbose
+def checkout_config(project_name, remote='origin'):
+    """Checkout a projects configuration branch.
+
+    """
+    git.assert_in_repository()
+    local('git fetch %(remote)s '
+          'refs/meta/config:refs/remotes/%(remote)s/meta/config' %
+          {'remote': remote})
+    local('git checkout meta/config')
+
+
+@task
+@verbose
+def push_config(project_name, remote='origin'):
+    """Push a projects configuration branch.
+
+    """
+    git.assert_in_repository()
+    local('git push %s meta/config:refs/for/refs/meta/config' % remote)
+
+
+@task
+@verbose
 def push_without_review(project_name, branch):
     """Push the given git branch to a remote gerrit repo."""
+    git.assert_in_repository()
     user = gitreview_username()
     local('git push ssh://%s@review.rc.nectar.org.au:29418/%s %s:refs/heads/%s' % (
           user, project_name, branch, branch))
