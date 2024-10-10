e650bf55109d339499a1f35e3ca7e9e69f3a43cd
Sam Morrison <sorrison@gmail.com>
2024-10-10 16:59:58 +1100

    Add some basic tests to common
    This highlights that it breaks on 3.8 but works on 3.10
    
    Change-Id: Ibd41a56c4dabcd4ac36a88400f87ec8af33dc7de

---
+++ hivemind/tests/test_common.py
@@ -0,0 +1,12 @@
+import unittest
+
+from hivemind import common
+
+
+class CommonTestCase(unittest.TestCase):
+
+    def test_list_commands(self):
+        common.list_commands()
+
+    def test_load_fabfile(self):
+        common._load_fabfile()
